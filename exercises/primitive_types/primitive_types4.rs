63e51d6a695aada523ab76975693df50b2fd50b4
Daniel Tognon <xone.tognon@outlook.com>
2023-01-20 15:05:31 -0300

    fix: primitive_types4

--- exercises/primitive_types/primitive_types4.rs
+++ exercises/primitive_types/primitive_types4.rs
@@ -8,7 +8,8 @@
 fn slice_out_of_array() {
     let a = [1, 2, 3, 4, 5];
 
-    let nice_slice = ???
+    // let nice_slice = [a[1], a[2], a[3]];
+    let nice_slice = &a[1..4];
 
     assert_eq!([2, 3, 4], nice_slice)
 }
