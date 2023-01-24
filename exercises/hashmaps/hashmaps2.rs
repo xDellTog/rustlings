80e16e1a9ca7ff5edbc7c9d58b72a3d56bc85f83
Daniel Tognon <xone.tognon@outlook.com>
2023-01-24 14:52:03 -0300

    fix: hashmaps2

--- exercises/hashmaps/hashmaps2.rs
+++ exercises/hashmaps/hashmaps2.rs
@@ -11,8 +11,6 @@
 //
 // Execute `rustlings hint hashmaps2` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 use std::collections::HashMap;
 
 #[derive(Hash, PartialEq, Eq)]
@@ -37,6 +35,13 @@ fn fruit_basket(basket: &mut HashMap<Fruit, u32>) {
         // TODO: Put new fruits if not already present. Note that you
         // are not allowed to put any type of fruit that's already
         // present!
+        if fruit == Fruit::Banana {
+            basket.insert(Fruit::Banana, 1);
+        }
+        
+        if fruit == Fruit::Pineapple {
+            basket.insert(Fruit::Pineapple, 1);
+        }
     }
 }
 
