e40dc901892376fbbdc7753c3edbb08375516476
Daniel Tognon <xone.tognon@outlook.com>
2023-01-25 15:19:20 -0300

    fix: hashmaps3

--- exercises/hashmaps/hashmaps3.rs
+++ exercises/hashmaps/hashmaps3.rs
@@ -14,11 +14,10 @@
 
 // Execute `rustlings hint hashmaps3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 use std::collections::HashMap;
 
 // A structure to store team name and its goal details.
+#[derive(Debug)]
 struct Team {
     name: String,
     goals_scored: u8,
@@ -40,7 +39,31 @@ fn build_scores_table(results: String) -> HashMap<String, Team> {
         // will be the number of goals conceded from team_2, and similarly
         // goals scored by team_2 will be the number of goals conceded by
         // team_1.
+        let scoreTeam1 = scores
+            .entry(team_1_name.clone())
+            .and_modify(|score| {
+                score.goals_scored += team_1_score;
+                score.goals_conceded += team_2_score;
+            })
+            .or_insert(Team {
+                name: team_1_name,
+                goals_scored: team_1_score,
+                goals_conceded: team_2_score,
+            });
+
+        let scoreTeam2 = scores
+            .entry(team_2_name.clone())
+            .and_modify(|score| {
+                score.goals_scored += team_2_score;
+                score.goals_conceded += team_1_score;
+            })
+            .or_insert(Team {
+                name: team_2_name,
+                goals_scored: team_2_score,
+                goals_conceded: team_1_score,
+            });
     }
+
     scores
 }
 
@@ -59,7 +82,8 @@ mod tests {
 
     #[test]
     fn build_scores() {
-        let scores = build_scores_table(get_results());
+        let results = get_results();
+        let scores = build_scores_table(results);
 
         let mut keys: Vec<&String> = scores.keys().collect();
         keys.sort();
